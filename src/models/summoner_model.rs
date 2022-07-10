359e95f4f710e5e95c4570d71b9783355519ff95
Paul COMTE <paul.codepro@gmail.com>
2022-07-10 12:03:22 +0200

    :sparkles: added new riot api unchecked + temporary summoner retriever

--- src/models/summoner_model.rs
+++ src/models/summoner_model.rs
@@ -1,9 +1,16 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default, Debug, PartialEq)]
 pub struct Summoner {
+    #[serde(alias = "accountId")]
     pub account_id: String,
+    #[serde(alias = "profileIconId")]
     pub profile_icon_id: i32,
+    #[serde(alias = "revisionDate")]
     pub revision_date: i64,
     pub name: String,
     pub id: String,
     pub puuid: String,
+    #[serde(alias = "summonerLevel")]
     pub summoner_level: i64,
 }
--- src/riot_api.rs
+++ src/riot_api.rs
@@ -1,4 +1,4 @@
-use crate::{platform::*, region::*};
+use crate::{models::summoner_model::*, platform::*, region::*};
 use ureq::serde_json;
 
 #[derive(Debug, PartialEq)]
@@ -7,7 +7,7 @@ pub struct RiotApi {
 }
 
 impl RiotApi {
-    /// Creates a new RiotApi with a token
+    /// Creates a new RiotApi with a token.
     /// It checks if the token is valid by retrieving the League of Legends NA1 region status.
     ///
     /// # Examples
@@ -35,6 +35,65 @@ impl RiotApi {
         }
         None
     }
+
+    /// Creates a new RiotApi with a token.
+    /// It doesn't check if the token is valid.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use samira::riot_api::*;
+    ///
+    /// let api = RiotApi::new_unchecked("TOKEN_HERE");
+    /// ```
+    pub fn new_unchecked(token: &str) -> RiotApi {
+        return RiotApi {
+            token: token.to_string(),
+        };
+    }
+
+    /// Retrieve a summoner by its puuid.
+    /// If the summoner does not exist it returns None.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use samira::{riot_api::*, platform::*};
+    ///
+    /// let api = RiotApi::new("RGAPI-db744840-2107-4a1c-931d-3a1e95b7ad8b").unwrap();
+    /// let puuid = "Y22N0dvmtG6NsF5GTpPJ4yhxI2t3zMvP5solMwWSqj1Ld-YAijBqMG5bDP9xYZ9EgVkyxiyifsMC_Q";
+    /// let summoner = api.get_summoner_by_puuid(&Platform::EUW1, &puuid);
+    /// assert_eq!(summoner.unwrap().puuid, puuid);
+    /// ```
+    pub fn get_summoner_by_puuid(&self, platform: &Platform, puuid: &str) -> Option<Summoner> {
+        let summoner_result = get_summoner_by_puuid(&self.token, platform, puuid);
+        if summoner_result.is_ok() {
+            return Some(summoner_result.unwrap());
+        }
+        None
+    }
+}
+
+fn get_summoner_by_puuid(
+    token: &str,
+    platform: &Platform,
+    puuid: &str,
+) -> Result<Summoner, ureq::Error> {
+    let request = format!(
+        "{server}/lol/summoner/v4/summoners/by-puuid/{puuid}",
+        server = get_platform_url(platform),
+        puuid = puuid
+    );
+    let response: serde_json::Value = ureq::get(&request)
+        .set("X-Riot-Token", token)
+        .call()?
+        .into_json()?;
+
+    Ok(serde_json::from_value(response).unwrap())
 }
 
 fn check_token(token: &str) -> Result<bool, ureq::Error> {
